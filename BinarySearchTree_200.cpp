eac80264c96700a20a8f97e36c36b000c4780d48
bagusplendut-alt <bagusplendut@gmail.com>
2026-06-12 15:11:03 +0700

    memberi nilai parameter ke variabel

--- BinarySearchTree_200.cpp
+++ BinarySearchTree_200.cpp
@@ -12,4 +12,10 @@ class Node {
 
         //membuat constructor untuk class node
     Node(string i, Node* l, Node* r) {
-        
+        //memberi nilai parameter ke variabel
+        info = i;
+        leftChild = l;
+        rightChild = r;
+    }
+
+};
