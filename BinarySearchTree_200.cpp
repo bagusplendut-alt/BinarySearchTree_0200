6a8c0747c5a7fcc7fa8ad86f6541a3a3469cfedb
bagusplendut-alt <bagusplendut@gmail.com>
2026-06-12 15:07:44 +0700

    memberikan akses publik

--- BinarySearchTree_200.cpp
+++ BinarySearchTree_200.cpp
@@ -3,3 +3,9 @@ using namespace std;
 
 //membuat class node
 class Node {
+
+    //memberi akses modifier serta mendeklarasi variabel dan object node
+    public :
+        string info;
+        Node* leftChild;
+        Node* rightChild;
